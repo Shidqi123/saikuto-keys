3e991650c7e92eff9cff8dce2532f6933cc5e938
Shidqi123 <99405928+Shidqi123@users.noreply.github.com>
2025-08-16 09:35:09 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+async function login() {
+  const keyInput = document.getElementById("keyInput").value.trim();
+  const message = document.getElementById("message");
+
+  try {
+    const response = await fetch("https://raw.githubusercontent.com/Shidqi123/saikuto-keys/main/keys.json");
+    const data = await response.json();
+
+    if (data.keys.includes(keyInput)) {
+      window.location.href = "success.html";
+    } else {
+      message.textContent = "KEY SUDAH KE PAKAI / SALAH, HARAP CHAT SAII UNTUK KIRIM KEY BARU :3";
+      message.style.color = "red";
+    }
+  } catch (error) {
+    message.textContent = "Gagal memeriksa key.";
+    message.style.color = "orange";
+  }
+}
